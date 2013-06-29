41f3f1b283fddd6baf7ba3be0119a50311505809
captconrado <ottey001@gmail.com>
2013-06-28 18:01:43 -0700

    added the rsvp

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,6 @@
 class Rsvp < ActiveRecord::Base
   attr_accessible :email, :going, :name
+  validates_uniqueness_of :email
+  validates_presence_of :name
+  # validates_inclusion_of :going, :in ; [true, false]
 end
